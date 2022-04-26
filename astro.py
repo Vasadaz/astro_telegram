82a9f39eb7da6a8e15984cccf16e808a0b43c1b6
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-04-26 15:08:04 +0300

    Restructuring of the "download_img" and "fetch_spacex_last_launch" functions

--- astro.py
+++ astro.py
@@ -3,13 +3,13 @@ from pathlib import Path
 import requests
 
 
-def download_img(url_img: str, save_path: str, img_name: str):
+def download_img(url_img: str, save_path: str):
     Path(save_path).mkdir(parents=True, exist_ok=True)
 
     response = requests.get(url_img)
     response.raise_for_status()
-
-    with open(f"{save_path}/{img_name}", 'wb') as file:
+    name_image = url_img.split("/")[-1]
+    with open(f"{save_path}/{name_image}", 'wb') as file:
         file.write(response.content)
 
 
@@ -18,8 +18,7 @@ def fetch_spacex_last_launch(id_launch: int):
     response.raise_for_status()
 
     for spacex_link_img in response.json()[id_launch]["links"]["flickr"]["original"]:
-        name_image = spacex_link_img.split("/")[-1]
-        download_img(spacex_link_img, "images", name_image)
+        download_img(spacex_link_img, "images")
 
 
 if __name__ == "__main__":
