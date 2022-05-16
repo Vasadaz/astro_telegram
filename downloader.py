9b53f275fcc36d17d911559f39652472694830bb
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-05-16 15:28:49 +0300

    Renamed the "parser_img_file" to "parse_url_img_file" function

--- downloader.py
+++ downloader.py
@@ -10,13 +10,13 @@ def download_img(url_img: str, save_path: str):
 
     response = requests.get(url_img)
     response.raise_for_status()
-    name_image = "".join(parser_img_file(url_img))
+    name_image = "".join(parse_url_img_file(url_img))
 
     with open(f"{save_path}/{name_image}", 'wb') as file:
         file.write(response.content)
 
 
-def parser_img_file(url: str) -> tuple:
+def parse_url_img_file(url: str) -> tuple:
     img_url_path = urllib.parse.urlsplit(url).path
     img_url_path_unquote = urllib.parse.unquote(img_url_path)
     img_file = os.path.split(img_url_path_unquote)[-1]
