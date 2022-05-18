996cc399d4f124deecf08d10d4f0c41370295b5e
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-05-18 21:07:27 +0300

    Renamed and optimization to "search_full_name_image" function

--- downloader.py
+++ downloader.py
@@ -7,19 +7,20 @@ import requests
 
 def download_img(url_img: str, save_path: str):
     Path(save_path).mkdir(parents=True, exist_ok=True)
+    name_img = search_full_name_image(url_img)
 
     response = requests.get(url_img)
     response.raise_for_status()
-    name_image = "".join(parse_url_img_file(url_img))
 
-    with open(f"{save_path}/{name_image}", 'wb') as file:
+    with open(f"{save_path}/{name_img}", 'wb') as file:
         file.write(response.content)
 
 
-def parse_url_img_file(url: str) -> tuple:
+def search_full_name_image(url: str) -> str:
     img_url_path = urllib.parse.urlsplit(url).path
     img_url_path_unquote = urllib.parse.unquote(img_url_path)
     img_file = os.path.split(img_url_path_unquote)[-1]
     img_file_tuple = os.path.splitext(img_file)
+    name_img = "".join(img_file_tuple)
 
-    return img_file_tuple
+    return name_img
